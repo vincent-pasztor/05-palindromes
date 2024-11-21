f63e49af51d08b0c0b2fc4952868dcd06a61e371
vincent-pasztor <vincent.pasztor@edu.esiee.fr>
2024-11-21 10:57:16 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
+"""
+Programme qui peut verifier si une chaine de caractere est un palindrome
+"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    """
+    Fonction secondaire "ispalindrome" qui verifie si la chaine de caractere est bien un palyndrome
+
+    args: chaine de char
+
+    return: true or false
+
+    """
 
     # votre code ici
-    
+    p = p.lower()
+    x = p.maketrans( "àâäéèêëîïôöùûüÿç" , "aaaeeeeiioouuuyc"," ?! :/',[].-_")
+    p = p.translate(x)
+
+    if p == p[::-1]:
+        return True
+
     return False
 
 #### Fonction principale
 
-
 def main():
+    """
+    Appele de fonction secondaire verifier si des chaines de caracter sont des palindromes
+
+    """
 
     # vos appels à la fonction secondaire ici
 
